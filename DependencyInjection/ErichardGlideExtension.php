2dac409a0894e5f605d3a1e65945a0b7c9811022
Michał Młodziński <mlodzinski+github@gmail.com>
2020-11-10 21:07:00 +0100

    Update ErichardGlideExtension.php

--- DependencyInjection/ErichardGlideExtension.php
+++ DependencyInjection/ErichardGlideExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
